e387ee2fca2ab55b5a40000c888cad5a6071ec4b
NIKHIL <63747907+nikhiljha261@users.noreply.github.com>
2021-07-14 19:54:49 +0530

    Create gamecoin.cpp

---
+++ gamecoin.cpp
@@ -0,0 +1,29 @@
+//har har mahadev
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define loop(i,a,b)     for(int i=(a);i<(b);i++)
+#define endl            "\n"
+
+void ganeshji(){
+	#ifndef ONLINE_JUDGE
+  	freopen("input.txt", "r", stdin);
+  	freopen("output.txt", "w", stdout);
+	#endif
+}
+
+int main()
+{
+	  ganeshji();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t=1;
+    // cin>>t;
+    while(t--)
+    {
+        
+      
+    }
+} 
